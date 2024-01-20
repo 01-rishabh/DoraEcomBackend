a990a3d9c9b62089437abf406ac0dd7cf9e73aa1
Rishabh Dora <rishabhd.1121@gmail.com>
2024-01-20 18:10:49 +0530

    Added Cors headers

--- app.js
+++ app.js
@@ -84,6 +84,17 @@ require('./config');
 // Set up middleware for parsing JSON
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+app.use((_, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*'); // or 'localhost:8888'
+  res.set('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  res.set(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  return next();
+}); // sets headers before routes
+
 // Use your product routes (update with your actual routes)
 app.use('/products', productRoutes);
 
